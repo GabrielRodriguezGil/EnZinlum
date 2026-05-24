ab4200b2d75e626dddd44ea72c71847bbca3fd5c
goskus23 <141594539+goskus23@users.noreply.github.com>
2026-05-24 23:03:55 +0200

    feat(Address): :sparkles: Añadir v1 clase Addres.

---
+++ app/src/main/java/edu/teamrocket/Address.java
@@ -0,0 +1,47 @@
+package edu.teamrocket;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class Address {
+    public PublicKey PK = null;
+    private PrivateKey SK = null;
+    private double balance = 0d;
+    private String symbol = "EZI";
+
+
+    Address(){
+    }
+
+    boolean isSKpresent(){
+        return SK != null;
+    }
+
+    public void generateKeyPair(){
+        KeyPair pair = GenSig.generateKeyPair();
+        this.SK = pair.getPrivate();
+        setPK(pair.getPublic());
+    }
+
+    void transferEZI(double enziniums){
+        this.balance += enziniums;
+    }
+
+    private void setPK(PublicKey PK){
+        this.PK = PK;
+    }
+
+    @Override
+    public String toString() {
+        return "PublicKey: " + this.PK.hashCode();
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public PublicKey getPK() {
+        return PK;
+    }
+}
